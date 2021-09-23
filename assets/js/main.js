11f360c66aef01d241b93b9ca04be0e09f4307d7
Tima Omega <tima_omega@outlook.com>
2021-09-24 01:46:07 +0300

    complete desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,37 @@
-document.querySelector('.international__more').addEventListener('click', function(){
-	document.querySelector('.international__wrap-text').classList.toggle('open')
-	document.querySelector('.international__text').classList.toggle('open')
-	if (this.textContent === '>> развернуть') {
-		this.textContent = '>> свернуть';
-	} else {
-		this.textContent = '>> развернуть'
+const more = document.querySelector('.international__more');
+
+if(more) {
+	more.addEventListener('click', function(){
+		document.querySelector('.international__wrap-text').classList.toggle('open')
+		document.querySelector('.international__text').classList.toggle('open')
+		if (this.textContent === '>> развернуть') {
+			this.textContent = '>> свернуть';
+		} else {
+			this.textContent = '>> развернуть'
+		}
+	});
+}
+
+document.addEventListener('scroll', function() {
+	const upBtn = document.querySelector('.up');
+	if(upBtn) {
+		if(window.scrollY > document.querySelector('.header').clientHeight) {
+			upBtn.style.display = 'flex';
+		} else {
+			upBtn.style.display = 'none';
+		}
 	}
+})
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 3,
+	spaceBetween: 20,
+	pagination: {
+		el: '.swiper-pagination',
+	  },
+	
+	  navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	  },
 });

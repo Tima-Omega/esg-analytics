accbf41c0e454c1aac9def0379dd8708b393c87c
Tima Omega <tima_omega@outlook.com>
2021-09-25 19:28:51 +0300

    done mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 const more = document.querySelector('.international__more');
+const burger = document.querySelector('.burger');
+const burgerList = document.querySelector('.burger__list')
+const upBtn = document.querySelector('.up');
+const choose = document.querySelector('.choose__trigger')
+const chooseList = document.querySelector('.choose__list')
+
+if(choose) {
+	choose.addEventListener('click', function(){
+		this.classList.toggle('open')
+		chooseList.classList.toggle('open')
+	});
+}
 
 if(more) {
 	more.addEventListener('click', function(){
@@ -12,13 +24,22 @@ if(more) {
 	});
 }
 
+
+
+burger.addEventListener('click', function() {
+	this.classList.toggle('open')
+	burgerList.classList.toggle('open')
+});
+
 document.addEventListener('scroll', function() {
-	const upBtn = document.querySelector('.up');
 	if(upBtn) {
 		if(window.scrollY > document.querySelector('.header').clientHeight) {
-			upBtn.style.display = 'flex';
+			upBtn.style.opacity = '1';
+			upBtn.style.visibility = 'visible';
 		} else {
-			upBtn.style.display = 'none';
+			upBtn.style.opacity = '0';
+			upBtn.style.visibility = 'hidden';
+
 		}
 	}
 })
@@ -35,3 +56,16 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-prev',
 	  },
 });
+
+function addAutoResize() {
+	document.querySelectorAll('[data-autoresize]').forEach(function (element) {
+	  element.style.boxSizing = 'border-box';
+	  const offset = element.offsetHeight - element.clientHeight;
+	  element.addEventListener('input', function (event) {
+		event.target.style.height = 'auto';
+		event.target.style.height = event.target.scrollHeight + offset + 'px';
+	  });
+	  element.removeAttribute('data-autoresize');
+	});
+  }
+addAutoResize();
